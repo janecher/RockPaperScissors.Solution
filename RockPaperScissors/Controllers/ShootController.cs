f6a586ac23690e44205db5782fed6db7fea0db3c
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-12-14 15:44:26 -0800

    implement GET request with name and play parameters to show string response with OK result and game result, and error message if request unsaccsessfull

---
+++ RockPaperScissors/Controllers/ShootController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RockPaperScissors.Models;
+
+namespace RockPaperScissors.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShootController : ControllerBase
+    {
+        const int httpOk = 200;
+        const int httpBadRequest = 400;
+        const int httpInternalError = 500;
+
+        // GET api/shoot
+        [HttpGet]
+        public ActionResult<Object> Get(string name, string play)
+        {
+            try
+            {
+                string gameResult = Player.GameResult(play);
+                string report = $"Player {name} {gameResult} the round";
+                return StatusCode(httpOk, report);
+            }
+            catch (ArgumentException ex)
+            {
+                return StatusCode(httpBadRequest, new ErrorMessage(ex.Message));
+            }
+            catch (Exception)
+            {
+                return StatusCode(httpInternalError,
+                                  new ErrorMessage("Internal error occured. Please contact evgeniya.chernaya@gmail.com"));
+            }
+        }
+    }
+}
