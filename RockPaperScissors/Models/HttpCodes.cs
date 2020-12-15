dbbc90789223472b643c081a2f7e3048ee2e1017
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-12-14 20:41:22 -0800

    create HttpCodes static class with constants for StatusCode

---
+++ RockPaperScissors/Models/HttpCodes.cs
@@ -0,0 +1,9 @@
+namespace RockPaperScissors.Models
+{
+  public static class HttpCodes
+  {
+    public const int Ok = 200;
+    public const int BadRequest = 400;
+    public const int InternalError = 500;
+  }
+}
