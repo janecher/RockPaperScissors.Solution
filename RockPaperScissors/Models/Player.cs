5e89816a021363fe91dbbd0eecc5e8c7886bdbb4
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-12-14 20:38:48 -0800

    refactor the Player class to remove Name property. constructor, made some fields and methods private, add parameter validation for PlayGame method

--- RockPaperScissors/Models/Player.cs
+++ RockPaperScissors/Models/Player.cs
@@ -5,37 +5,42 @@ namespace RockPaperScissors.Models
 {
   public class Player
   {
-    public string Name {get; set;}
     private static Dictionary<string, int> _playersList = new Dictionary<string, int>();
-    public Player(string name)
-    {
-      Name = name;
-    }
-    public static string[] plays = new string[3]{"rock", "paper", "scissors"};
+    private static string[] plays = new string[3]{"rock", "paper", "scissors"};
 
-    public static string RandomPlay()
+    private static string RandomPlay()
     {
       Random rand = new Random();
       int randomPlay = rand.Next(3); 
       return plays[randomPlay];
     }
-
-    public static string GameResult(string play, string name)
+    
+    public static string PlayGame(string play, string player_name)
     {
+      if(String.IsNullOrEmpty(player_name) || String.IsNullOrEmpty(play))
+      {
+        throw new ArgumentException("player_name and play parameters are required");
+      }
+      if(Array.IndexOf(plays, play) == -1)
+      {
+        throw new ArgumentException("play parameter is invalid");
+      }
       string randomPlay = RandomPlay();
       if(play == randomPlay)
       {
         return "ties";
       }
-      if(play == "rock" && randomPlay == "scissors" || play == "scissors" && randomPlay == "paper" || play == "paper" && randomPlay == "rock")
+      if(play == "rock" && randomPlay == "scissors" ||
+         play == "scissors" && randomPlay == "paper" ||
+         play == "paper" && randomPlay == "rock")
       {
-        if(_playersList.ContainsKey(name))
+        if(_playersList.ContainsKey(player_name))
         {
-          _playersList[name]++;
+          _playersList[player_name]++;
         }
         else
         {
-          _playersList.Add(name, 1);
+          _playersList.Add(player_name, 1);
         }
         return "wins";
       }
