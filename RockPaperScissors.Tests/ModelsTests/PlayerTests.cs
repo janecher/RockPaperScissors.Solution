077671f9650af70c34082a97a88f34d33b38c3d7
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-12-15 22:22:04 -0800

    create test for Player class PlayGame method, test passed

--- RockPaperScissors.Tests/ModelsTests/PlayerTests.cs
+++ RockPaperScissors.Tests/ModelsTests/PlayerTests.cs
@@ -11,6 +11,66 @@ namespace RockPaperScissors.Tests
   [TestClass]
   public class PlayerTests
   {
+    // class RandomGeneratorTest : RandomGenerator
+    // {
+    //   public override int GetNumber(int maxValue)
+    //   {
+    //     return 2;
+    //   }
+    // }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException),
+    "player_name and play parameters are required")]
+    public void PlayGame_EmptyParameters_ArgumentException()
+    {
+      Player player = new Player(new RandomGenerator());
+      player.PlayGame("", "");
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException),
+    "play parameter is invalid")]
+    public void PlayGame_InvalidPlay_ArgumentException()
+    {
+      Player player = new Player(new RandomGenerator());
+      player.PlayGame("roccs", "Evgeniya");
+    }
+
+    [TestMethod]
+    public void PlayGame_BothScissors_Ties()
+    {
+      Mock<RandomGenerator> randomTest = new Mock<RandomGenerator>(MockBehavior.Strict);
+      randomTest.Setup(h => h.GetNumber(It.Is<int>(t => t == 3)))
+        .Returns(2);
+      Player player = new Player(randomTest.Object);
+      Assert.AreEqual("ties", player.PlayGame("scissors", "Evgeniya"));
+
+      randomTest.VerifyAll();
+    }
     
+    [TestMethod]
+    public void PlayGame_RockScissors_Wins()
+    {
+      Mock<RandomGenerator> randomTest = new Mock<RandomGenerator>(MockBehavior.Strict);
+      randomTest.Setup(h => h.GetNumber(It.Is<int>(t => t == 3)))
+        .Returns(2);
+      Player player = new Player(randomTest.Object);
+      Assert.AreEqual("wins", player.PlayGame("rock", "Evgeniya"));
+
+      randomTest.VerifyAll();
+    }
+
+    [TestMethod]
+    public void PlayGame_PaperScissors_Ties()
+    {
+      Mock<RandomGenerator> randomTest = new Mock<RandomGenerator>(MockBehavior.Strict);
+      randomTest.Setup(h => h.GetNumber(It.Is<int>(t => t == 3)))
+        .Returns(2);
+      Player player = new Player(randomTest.Object);
+      Assert.AreEqual("loses", player.PlayGame("paper", "Evgeniya"));
+
+      randomTest.VerifyAll();
+    }
   }
 }
