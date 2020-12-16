f63c56a1c12f4befb49e83fe54dab8e792eca5d8
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-12-15 20:38:05 -0800

    set testing for the project

---
+++ RockPaperScissors.Tests/ModelsTests/PlayerTests.cs
@@ -0,0 +1,16 @@
+using RockPaperScissors.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RockPaperScissors.Tests
+{
+  [TestClass]
+  public class PlayerTests
+  {
+    
+  }
+}
