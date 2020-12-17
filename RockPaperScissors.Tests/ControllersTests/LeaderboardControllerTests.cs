db7a1d35cc710178ada1263fd39c126df24e5d4e
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-12-16 21:50:07 -0800

    add VerufyAll to the test and remove Mock Setup for two tests where mock object was not used

--- RockPaperScissors.Tests/ControllersTests/LeaderboardControllerTests.cs
+++ RockPaperScissors.Tests/ControllersTests/LeaderboardControllerTests.cs
@@ -19,28 +19,30 @@ namespace RockPaperScissors.Tests
     [TestMethod]
     public void GetLeaderboard_Ties_Ok()
     {
-        // Arrange
-        Mock<RandomGenerator> randomTest = new Mock<RandomGenerator>(MockBehavior.Strict);
-        randomTest.Setup(h => h.GetNumber(It.Is<int>(t => t == 3)))
-          .Returns(2);
-        ShootController controller = new ShootController(randomTest.Object);
-        controller.Get("rock", "Jane");
-        controller.Get("rock", "Evgeniya");
-        controller.Get("rock", "Evgeniya");
-        controller.Get("scissors", "Jane");
-        controller.Get("paper", "Evgeniya");
+      // Arrange
+      Mock<RandomGenerator> randomTest = new Mock<RandomGenerator>(MockBehavior.Strict);
+      randomTest.Setup(h => h.GetNumber(It.Is<int>(t => t == 3)))
+        .Returns(2);
+      ShootController controller = new ShootController(randomTest.Object);
+      controller.Get("rock", "Jane");
+      controller.Get("rock", "Evgeniya");
+      controller.Get("rock", "Evgeniya");
+      controller.Get("scissors", "Jane");
+      controller.Get("paper", "Evgeniya");
 
-        LeaderboardController leadController = new LeaderboardController();
-        // Act
-        var actionResult = leadController.Get();
-        var objectResult = actionResult.Result as ObjectResult;
+      LeaderboardController leadController = new LeaderboardController();
+      // Act
+      var actionResult = leadController.Get();
+      var objectResult = actionResult.Result as ObjectResult;
 
-        var listResult = new List<PlayerInfo>(){new PlayerInfo{PlayerName = "Evgeniya", Score = 2}, new PlayerInfo{PlayerName = "Jane", Score = 1}};
+      var listResult = new List<PlayerInfo>(){new PlayerInfo{PlayerName = "Evgeniya", Score = 2}, new PlayerInfo{PlayerName = "Jane", Score = 1}};
 
-        //Assert
-        Assert.IsNotNull(objectResult);
-        Assert.AreEqual(200, objectResult.StatusCode);
-        Assert.AreEqual(JsonConvert.SerializeObject(listResult, Formatting.Indented), objectResult.Value);
+      //Assert
+      Assert.IsNotNull(objectResult);
+      Assert.AreEqual(200, objectResult.StatusCode);
+      Assert.AreEqual(JsonConvert.SerializeObject(listResult, Formatting.Indented), objectResult.Value);
+
+      randomTest.VerifyAll();
     }
   }
 }
--- RockPaperScissors.Tests/ControllersTests/ShootControllerTests.cs
+++ RockPaperScissors.Tests/ControllersTests/ShootControllerTests.cs
@@ -17,81 +17,85 @@ namespace RockPaperScissors.Tests
     [TestMethod]
     public void GetShoot_Ties_Ok()
     {
-        // Arrange
-        Mock<RandomGenerator> randomTest = new Mock<RandomGenerator>(MockBehavior.Strict);
-        randomTest.Setup(h => h.GetNumber(It.Is<int>(t => t == 3)))
-          .Returns(2);
-        ShootController controller = new ShootController(randomTest.Object);
+      // Arrange
+      Mock<RandomGenerator> randomTest = new Mock<RandomGenerator>(MockBehavior.Strict);
+      randomTest.Setup(h => h.GetNumber(It.Is<int>(t => t == 3)))
+        .Returns(2);
+      ShootController controller = new ShootController(randomTest.Object);
 
-        // Act
-        var actionResult = controller.Get("scissors", "Evgeniya");
-        var objectResult = actionResult.Result as ObjectResult;
+      // Act
+      var actionResult = controller.Get("scissors", "Evgeniya");
+      var objectResult = actionResult.Result as ObjectResult;
 
-        //Assert
-        Assert.IsNotNull(objectResult);
-        Assert.AreEqual(200, objectResult.StatusCode);
-        Assert.AreEqual("Player Evgeniya ties the round", objectResult.Value);
+      //Assert
+      Assert.IsNotNull(objectResult);
+      Assert.AreEqual(200, objectResult.StatusCode);
+      Assert.AreEqual("Player Evgeniya ties the round", objectResult.Value);
+
+      randomTest.VerifyAll();
     }
 
     [TestMethod]
     public void GetShoot_Wins_Ok()
     {
-        // Arrange
-        Mock<RandomGenerator> randomTest = new Mock<RandomGenerator>(MockBehavior.Strict);
-        randomTest.Setup(h => h.GetNumber(It.Is<int>(t => t == 3)))
-          .Returns(2);
-        ShootController controller = new ShootController(randomTest.Object);
+      // Arrange
+      Mock<RandomGenerator> randomTest = new Mock<RandomGenerator>(MockBehavior.Strict);
+      randomTest.Setup(h => h.GetNumber(It.Is<int>(t => t == 3)))
+        .Returns(2);
+      ShootController controller = new ShootController(randomTest.Object);
 
-        // Act
-        var actionResult = controller.Get("rock", "Evgeniya");
-        var objectResult = actionResult.Result as ObjectResult;
+      // Act
+      var actionResult = controller.Get("rock", "Evgeniya");
+      var objectResult = actionResult.Result as ObjectResult;
 
-        //Assert
-        Assert.IsNotNull(objectResult);
-        Assert.AreEqual(200, objectResult.StatusCode);
-        Assert.AreEqual("Player Evgeniya wins the round", objectResult.Value);
+      //Assert
+      Assert.IsNotNull(objectResult);
+      Assert.AreEqual(200, objectResult.StatusCode);
+      Assert.AreEqual("Player Evgeniya wins the round", objectResult.Value);
+
+      randomTest.VerifyAll();
     }
 
     [TestMethod]
     public void GetShoot_EmptyPlay_BadRequest()
     {
-        // Arrange
-        Mock<RandomGenerator> randomTest = new Mock<RandomGenerator>(MockBehavior.Strict);
-        randomTest.Setup(h => h.GetNumber(It.Is<int>(t => t == 3)))
-          .Returns(2);
-        ShootController controller = new ShootController(randomTest.Object);
+      // Arrange
+      Mock<RandomGenerator> randomTest = new Mock<RandomGenerator>(MockBehavior.Strict);
+      ShootController controller = new ShootController(randomTest.Object);
 
-        // Act
-        var actionResult = controller.Get("", "Evgeniya");
-        var objectResult = actionResult.Result as ObjectResult;
+      // Act
+      var actionResult = controller.Get("", "Evgeniya");
+      var objectResult = actionResult.Result as ObjectResult;
 
-        var errorMessage = objectResult.Value as ErrorMessage;
+      var errorMessage = objectResult.Value as ErrorMessage;
 
-        //Assert
-        Assert.IsNotNull(objectResult);
-        Assert.AreEqual(400, objectResult.StatusCode);
-        Assert.AreEqual("player_name and play parameters are required", errorMessage.Error);
+      //Assert
+      Assert.IsNotNull(objectResult);
+      Assert.AreEqual(400, objectResult.StatusCode);
+      Assert.AreEqual("player_name and play parameters are required", errorMessage.Error);
+
+      randomTest.VerifyAll();
     }
 
     [TestMethod]
     public void GetShoot_IncorrectPlay_BadRequest()
     {
-        // Arrange
-        Mock<RandomGenerator> randomTest = new Mock<RandomGenerator>(MockBehavior.Strict);
-        randomTest.Setup(h => h.GetNumber(It.Is<int>(t => t == 3)))
-          .Returns(2);
-        ShootController controller = new ShootController(randomTest.Object);
+      // Arrange
+      Mock<RandomGenerator> randomTest = new Mock<RandomGenerator>(MockBehavior.Strict);
+      ShootController controller = new ShootController(randomTest.Object);
 
-        // Act
-        var actionResult = controller.Get("play", "Evgeniya");
-        var objectResult = actionResult.Result as ObjectResult;
+      // Act
+      var actionResult = controller.Get("play", "Evgeniya");
+      var objectResult = actionResult.Result as ObjectResult;
 
-        var errorMessage = objectResult.Value as ErrorMessage;
+      var errorMessage = objectResult.Value as ErrorMessage;
 
-        //Assert
-        Assert.IsNotNull(objectResult);
-        Assert.AreEqual(400, objectResult.StatusCode);
-        Assert.AreEqual("play parameter is invalid", errorMessage.Error);
+      //Assert
+      Assert.IsNotNull(objectResult);
+      Assert.AreEqual(400, objectResult.StatusCode);
+      Assert.AreEqual("play parameter is invalid", errorMessage.Error);
+
+      randomTest.VerifyAll();
     }
   }
 }
