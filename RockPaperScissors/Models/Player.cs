f105176ea904943348935d04ed2244b628b36b43
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-12-17 13:45:15 -0800

    add functionality to save game results in a Leaderboard.json file and get data from session to session

--- RockPaperScissors/Models/Player.cs
+++ RockPaperScissors/Models/Player.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-
+using System.IO;
+using Newtonsoft.Json;
 namespace RockPaperScissors.Models
 {
   public class Player
   {
+    private const string leaderboardFileName = "Leaderboard.json";
     private const string rock = "rock";
     private const string paper = "paper";
     private const string scissors = "scissors";
@@ -13,13 +15,19 @@ namespace RockPaperScissors.Models
     private const string wins = "wins";
     private const string loses = "loses";
     private string[] plays = new string[3]{rock, paper, scissors};
-    private static Dictionary<string, int> _playersList = new Dictionary<string, int>();
+    private static Dictionary<string, int> _playersList;
 
-    public RandomGenerator RandomPlay {get; set;}
+    public RandomGenerator _randomPlay {get; set;}
+
+    static Player()
+    {
+      _playersList = PlayersList();
+    }
 
     public Player(RandomGenerator rand)
     {
-      RandomPlay = rand;
+      _randomPlay = rand;
+      _playersList = PlayersList();
     }
 
     public string PlayGame(string play, string player_name)
@@ -33,7 +41,7 @@ namespace RockPaperScissors.Models
         throw new ArgumentException("play parameter is invalid");
       }
 
-      string randomPlay = plays[RandomPlay.GetNumber(plays.Length)];
+      string randomPlay = plays[_randomPlay.GetNumber(plays.Length)];
 
       if(play == randomPlay)
       {
@@ -51,6 +59,11 @@ namespace RockPaperScissors.Models
         {
           _playersList.Add(player_name, 1);
         }
+        using (StreamWriter file = File.CreateText(leaderboardFileName))
+        {
+            JsonSerializer serializer = new JsonSerializer();
+            serializer.Serialize(file, _playersList);
+        }
         return wins;
       }
       else
@@ -62,6 +75,22 @@ namespace RockPaperScissors.Models
     public static List<PlayerInfo> GetPlayersList()
     {
       return _playersList.OrderByDescending(key => key.Value).Select(playerInfo => new PlayerInfo { PlayerName = playerInfo.Key, Score = playerInfo.Value}).ToList();
+    }
+
+    private static Dictionary<string, int> PlayersList()
+    {
+      try
+      {
+        using (StreamReader reader = new StreamReader(leaderboardFileName))
+        {
+            string json = reader.ReadToEnd();
+            return JsonConvert.DeserializeObject<Dictionary<string, int>>(json);
+        }
+      }
+      catch
+      {
+        return new Dictionary<string, int>();
+      }
     }
   }
 }
